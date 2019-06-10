5a5906910e15b6fb38a8cf73679aa56bee85e547
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-10 16:39:33 +0000

    Done.

--- db/migrate/20190610054658_create_movies.rb
+++ db/migrate/20190610054658_create_movies.rb
@@ -8,3 +8,4 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.boolean  :in_theaters
   end
 end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_06_10_054658) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
